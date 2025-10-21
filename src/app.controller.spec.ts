a91effdc837a040bdc0ded03d096725f3fbfeace
hafii87 <muhammadhafeezurrehman77@gmail.com>
2025-10-21 19:50:25 +0500

    Added Jest test cases for CarService

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Car Booking System API is running!"', () => {
+      expect(appController.getHello()).toBe('Car Booking System API is running!');
     });
   });
 });
